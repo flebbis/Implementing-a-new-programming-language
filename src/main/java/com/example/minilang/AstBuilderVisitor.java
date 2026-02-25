616c09c824a044fd7cbb7f47c027d338d1743cc5
Emma Dahlqvist <ev.dahlqvist@gmail.com>
2026-02-25 13:50:19 +0100

    Issue #6: Add AST class and nodes

--- src/main/java/com/example/minilang/AstBuilderVisitor.java
+++ src/main/java/com/example/minilang/AstBuilderVisitor.java
@@ -5,7 +5,10 @@ import com.example.minilang.GrammarParser.*;
 /**
  * Transforms the raw Parse Tree (CST) into our nice, clean AST.
  */
-public class AstBuilderVisitor extends GrammarBaseVisitor<Ast.Exp> {
+
+import com.example.minilang.Ast.*;
+import java.util.stream.Collectors;
+
+public class AstBuilderVisitor extends GrammarBaseVisitor<Object> {
 
 }
-
---
+++ src/main/java/com/example/minilang/Pos.java
@@ -0,0 +1,20 @@
+﻿package com.example.minilang;
+
+public class Pos {
+    private final int line;
+    private final int column;
+
+    public Pos(int line, int column) {
+        this.line = line;
+        this.column = column;
+    }
+
+    public int getLine()  {
+        return line;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+}
+
