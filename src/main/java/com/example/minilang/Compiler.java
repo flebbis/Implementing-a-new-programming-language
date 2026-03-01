9818f9edfbab96c089111ee48b6e8ee4491af3d3
felixtan <felix3123@hotmail.com>
2026-03-01 23:18:24 +0100

    add live compilaton from llvm ir

--- src/main/java/com/example/minilang/Compiler.java
+++ src/main/java/com/example/minilang/Compiler.java
@@ -1,12 +1,15 @@
 package com.example.minilang;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-/*
-Test File, ville bara se att det printar ut// 
-LLVM ska lösa detta
-*/
+import java.nio.file.Path;
+
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
 public class Compiler {
 
     public static void main(String[] args) throws Exception {
@@ -14,6 +17,8 @@ public class Compiler {
             System.err.println("Usage: java -jar compiler.jar <sourcefile>");
             System.exit(1);
         }
+        parseFile(Path.of(args[0]));
+        /* 
         // läser rad för rad för filen
         String sourceFile = args[0];
         List<String> lines = Files.readAllLines(Paths.get(sourceFile));
@@ -32,22 +37,10 @@ public class Compiler {
         }
 
         System.out.print(sb);
+        */
     }
-}
-package com.example.minilang;
 
-import org.antlr.v4.runtime.BailErrorStrategy;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-public class Compiler {
-
-    public static void parseFile(Path path) throws IOException {
+     public static void parseFile(Path path) throws IOException {
         String input = Files.readString(path);
 
         // 2. Infrastructure
@@ -71,5 +64,28 @@ public class Compiler {
         AstBuilderVisitor astBuilder = new AstBuilderVisitor();
         Ast.Program astRoot = astBuilder.visit(tree);
         System.out.println("AST:      " + astRoot);
+
+        // generate llvm ir to a string
+        CodeGenerator codeGen = new CodeGenerator();
+        String ir = codeGen.generate(astRoot);
+        // a new os process that can process the llvm ir, llc which is for assembly and -filetype=asm 
+        // what llc should output which is assembly
+        ProcessBuilder pb = new ProcessBuilder("llc", "-filetype=asm");
+        //starts a process
+        Process process = pb.start();
+
+        //llc stdin gerenate LLVm, convert it to bytes and then close it 
+        process.getOutputStream().write(ir.getBytes());
+        process.getOutputStream().close();
+
+        // creats a Inputstream and read the assembly from llc stdout, convert it to string 
+        InputStream is = process.getInputStream();
+        byte[] bytes = is.readAllBytes();
+        String assembly = new String(bytes,StandardCharsets.UTF_8);
+
+        System.out.print(assembly);
+        
+        };
     }
-}
+    
+
