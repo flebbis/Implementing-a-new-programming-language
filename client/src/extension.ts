0300f369df6de4788fa5f018ae0fee0546b3e2cc
felixtan <felix3123@hotmail.com>
2026-03-10 11:31:47 +0100

    changed commands_opt to root package.json

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -134,12 +134,18 @@ export function activate(context: ExtensionContext) {
       const asmFile = lastPath.replace('.ml', '.s');
       execFileSync('llc', ['-filetype=asm', optLevel, llFile, '-o', asmFile]);
 
+      
+
       // read the assembly file
       const fs = require('fs');
       const asm = fs.readFileSync(asmFile, 'utf8');
+
+      fs.unlinkSync(llFile);
+      fs.unlinkSync(asmFile);
       const filtered = asm.split('\n')
       .filter((line: string) => !line.trim().startsWith('.'))
       .filter((line: string) => !line.trim().startsWith(';'))
+      .filter((line: string) => !line.trim().startsWith('l_'))
       .filter((line: string) => line.trim() !== '')
       .join('\n');
       asmProvider.setContent(filtered);
