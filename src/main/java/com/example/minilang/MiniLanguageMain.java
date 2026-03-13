fea3a0bdd41380a7b7dea512e3931dbb7f41a790
Emma Dahlqvist <ev.dahlqvist@gmail.com>
2026-03-13 09:37:47 +0100

    Update default test file path for type checking

--- src/main/java/com/example/minilang/MiniLanguageMain.java
+++ src/main/java/com/example/minilang/MiniLanguageMain.java
@@ -15,7 +15,7 @@ public class MiniLanguageMain {
         String testFilePath = "src/test/resources/";
 
         if(args.length == 0) {
-             path = testFilePath + "type/bad/add-bad.ml";
+             path = testFilePath + "type/bad/if-1.ml";
         } else if (args.length == 1){
              path = args[0];
         } else {
