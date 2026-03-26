434b586b9f5dbde38df98b0d88ecc0aee500deb4
felixtan <felix3123@hotmail.com>
2026-03-26 17:57:57 +0100

    fix

---
+++ client/src/architeture.ts
@@ -0,0 +1,11 @@
+export interface architecture {
+    name: string;
+    patterns: RegExp[];
+}
+
+export const PROFILES = {
+    "x86-64": {name: "x86-64", patterns: [/^\s*retq?/]},
+    "x86": {name: "x86", patterns: [/^\s*ret/]},
+    "aarch64": {name: "aarch64",  patterns: [/^\s*sub\s+sp,\s+sp,/, /^\s*add\s+sp,\s+sp,/,/^\s*ret/]},
+    "arm": {name: "arm", patterns: []}
+}
